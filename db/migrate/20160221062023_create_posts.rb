6d8c767f8051ec55408fe5ae2d2269cc606f187e
Antoun Sawires <antounsawires@gmail.com>
2016-02-21 02:00:26 -0500

    Changed Everything, Somthing greae Coming

--- db/migrate/20160221062023_create_posts.rb
+++ db/migrate/20160221062023_create_posts.rb
@@ -2,9 +2,9 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :title
-      t.text :content
+      t.text :body
 
-      t.timestamps null: false
+      t.timestamps 
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230222609) do
+ActiveRecord::Schema.define(version: 20160221062023) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
